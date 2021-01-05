5362309c14b30a22ca996a20a2cbc2f9d4ffe7d4
zorazky <oswaldo.vazquez@gmail.com>
2021-01-05 14:06:41 -0600

    summary and content boxes seems working fine

--- js/app.js
+++ js/app.js
@@ -33,3 +33,90 @@ function openCloseDropdown(event) {
         }
     }
 }
+var options = {
+    series: [
+        {
+            name: "Net Profit",
+            data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+        },
+        {
+            name: "Revenue",
+            data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+        },
+        {
+            name: "Free Cash Flow",
+            data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+        },
+    ],
+    chart: {
+        type: "bar",
+        height: 250,
+        sparkline: {
+            enabled: true,
+        },
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: "55%",
+            endingShape: "rounded",
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"],
+    },
+    xaxis: {
+        categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+    },
+    yaxis: {
+        title: {
+            text: "$ (thousands)",
+        },
+    },
+    fill: {
+        opacity: 1,
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return "$ " + val + " thousands";
+            },
+        },
+    },
+};
+
+var chart = new ApexCharts(document.querySelector("#apex1"), options);
+chart.render();
+
+
+var options2 = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+        type: 'donut',
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var chart2 = new ApexCharts(document.querySelector("#apex2"), options2);
+chart2.render();
+
+var chart3 = new ApexCharts(document.querySelector("#apex3"), options2);
+chart3.render();
+
+var chart4 = new ApexCharts(document.querySelector("#apex4"), options2);
+chart4.render();
